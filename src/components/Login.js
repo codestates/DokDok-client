728eb8827600fab9e39035270627de2c1376b059
Lee Soim <hande.sof@gmail.com>
2021-05-17 13:55:17 +0900

    feat: add router

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+const Login = () => {
+  return (
+    <div>
+      <p>Login</p>
+    </div>
+  );
+};
+
+export default withRouter(Login);
---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+const Main = () => {
+  return (
+    <div>
+      <p>main</p>
+    </div>
+  );
+};
+
+export default withRouter(Main);
