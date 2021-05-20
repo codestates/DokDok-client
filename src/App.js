09849e4f4cb7669429be61f80840b2b9cd1231e7
do0ogz <choi2411554@gmail.com>
2021-05-20 16:48:14 +0900

    feat:login,signup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { useSelector } from 'react-redux';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import './App.css';
 import Main from './pages/Main';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Goole_login from './components/Goole_login';
+import UpdateUserInfo from './components/UpdateUserInfo';
+import UserInfo from './components/UserInfo';
+// import App1 from './components/App1';
 
 const App = () => {
   const loginInfo = useSelector((state) => state.userReducer);
@@ -15,6 +21,12 @@ const App = () => {
     <div className="App">
       <Switch>
         <Route exact path="/main" render={() => <Main />} />
+        <Route exact path="/login" render={() => <Login />} />
+        <Route exact path="/signup" render={() => <Signup />} />
+        <Route exact path="/updateUserInfo" render={() => <UpdateUserInfo />} />
+        <Route exact path="/userInfo" render={() => <UserInfo />} />
+        <Route exact path="/goole_login" render={() => <Goole_login />} />
+        {/* <Route exact path="/App1" render={() => <App1 />} /> */}
       </Switch>
     </div>
   );
---
+++ src/components/Signup.js
@@ -0,0 +1,121 @@
+import '../scss/Signup.scss';
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+
+const Signup = ({ history }) => {
+  const {
+    register,
+    watch,
+    reset,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ mode: 'onChange' });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    //악시오스 로그인 요청
+    axios
+      .post(
+        process.env.REACT_APP_API_URL + '/users/signup',
+        {
+          email: data.email,
+          password: data.password,
+          ncikname: data.ncikname,
+        },
+        {
+          'Content-Type': 'application/json',
+        },
+      )
+      .then(function () {
+        console.log('회원가입 성공');
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+    reset();
+  };
+  return (
+    <div className="signupBox">
+      <h2>로고</h2>
+      <h1>Signup</h1>
+
+      <form className="signup-input-box" onSubmit={handleSubmit(onSubmit)}>
+        <div>Email</div>
+        <label className="input-box">
+          <input
+            placeholder="email"
+            {...register('email', {
+              required: '이메일을 입력해주세요.',
+              pattern: {
+                value:
+                  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                message: '잘못된 이메일 형식입니다.',
+              },
+            })}
+          />
+          {errors.email && <p>{errors.email.message}</p>}
+        </label>
+
+        <div>Nickname</div>
+        <label className="input-box">
+          <input
+            placeholder="nickname"
+            {...register('nickname', {
+              required: '닉네임을 입력해주세요.',
+              maxLength: {
+                value: 10,
+                message: '10자 미만으로 설정해주세요.',
+              },
+            })}
+          />
+          {errors.nickname && <p>{errors.nickname.message}</p>}
+        </label>
+
+        <div>Password</div>
+        <label className="input-box">
+          <input
+            placeholder="password"
+            type="password"
+            {...register('password', {
+              required: '비밀번호를 입력해주세요.',
+              minLength: {
+                value: 6,
+                message: '비밀번호는 최소 6자 이상입니다.',
+              },
+            })}
+          />
+          {errors.password && <p>{errors.password.message}</p>}
+        </label>
+
+        <div>Confirm Password</div>
+        <label className="input-box">
+          <input
+            placeholder="confirm password"
+            type="password"
+            {...register('confirm_password', {
+              required: '비밀번호를 다시 입력해주세요.',
+              validate: (value) =>
+                value === watch('password') || '비밀번호가 일치하지 않습니다',
+            })}
+          />
+          {errors.confirm_password && <p>{errors.confirm_password.message}</p>}
+        </label>
+
+        <button className="signup-btn" type="submit">
+          Signup
+        </button>
+      </form>
+
+      <div className="login-btn-area">
+        <label>You have an account?</label>
+        <button className="login-btn" onClick={() => history.push('/login')}>
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(Signup);
